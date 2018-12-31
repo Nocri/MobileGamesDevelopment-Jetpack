3c7237deca1e09ccbf72d076ad7833e51e330bc8
Mikołaj Karwowski <mikkarwowski@gmail.com>
2018-12-31 18:37:11 +0100

    first commit

---
+++ www/example_game.js
@@ -0,0 +1,52 @@
+/* Author: Derek O Reilly, Dundalk Institute of Technology, Ireland.             */
+/* There should always be a javaScript file with the same name as the html file. */
+/* This file always holds the playGame function().                               */
+/* It also holds game specific code, which will be different for each game       */
+
+
+
+
+
+/******************** Declare game specific global data and functions *****************/
+/* images must be declared as global, so that they will load before the game starts  */
+
+/******************* END OF Declare game specific data and functions *****************/
+
+
+//ToDo set proper image
+let birdImage = new Image();
+birdImage.src = "../res/sprites/player_runing.png";
+
+
+/* Always have a playGame() function                                     */
+/* However, the content of this function will be different for each game */
+function playGame()
+{
+    /* We need to initialise the game objects outside of the Game class */
+    /* This function does this initialisation.                          */
+    /* Specifically, this function will:                                */
+    /* 1. initialise the canvas and associated variables                */
+    /* 2. create the various game gameObjects,                   */
+    /* 3. store the gameObjects in an array                      */
+    /* 4. create a new Game to display the gameObjects           */
+    /* 5. start the Game                                                */
+
+
+
+    /* Create the various gameObjects for this game. */
+    /* This is game specific code. It will be different for each game, as each game will have it own gameObjects */
+
+    /* END OF game specific code. */
+
+
+    /* Always create a game that uses the gameObject array */
+    let game = new CanvasGame();
+
+    gameObjects[0] = new Explosion(birdImage, 250, 250, 150 )
+
+    /* Always play the game */
+    game.start();
+
+
+    /* If they are needed, then include any game-specific mouse and keyboard listners */
+}
